32f556c3360e4b336443d24e42ba9f6ca968195b
gerryfletch <1997Gerry@gmail.com>
2019-12-10 13:15:01 +0000

    Attempting to fix react-native-navigation

---
+++ src/authentication/LoginScreen.js
@@ -0,0 +1,15 @@
+/**
+ * @flow
+ */
+
+import React from "react";
+import {Text} from "react-native";
+
+const LoginComponent = () => <Text>Hello</Text>;
+
+LoginComponent.navigationOptions = {
+    title: 'Login',
+    header: null
+};
+
+export default LoginComponent;
