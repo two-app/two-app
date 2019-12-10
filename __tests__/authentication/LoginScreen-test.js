ef83f11d7da1b93251af17a259e6ea298a8b047c
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2019-12-10 14:02:42 +0000

    Synced package lock (#2)

---
+++ __tests__/authentication/LoginScreen-test.js
@@ -0,0 +1,14 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+import LoginScreen from "../../src/authentication/LoginScreen";
+
+it('should render correctly', () => {
+    expect(renderer.create(<LoginScreen/>).toJSON()).toMatchSnapshot();
+});
