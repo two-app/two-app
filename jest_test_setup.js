4489b85d1cb06d039e88269775d9ad8f048fbedb
gerryfletch <1997Gerry@gmail.com>
2019-12-10 18:08:28 +0000

    Wrote tests for login screen

--- jest_test_setup.js
+++ jest_test_setup.js
@@ -4,3 +4,8 @@ import Adapter from 'enzyme-adapter-react-16';
 import Enzyme from 'enzyme';
 
 Enzyme.configure({ adapter: new Adapter() });
+
+// Ignore React Web errors when using React Native
+console.error = message => {
+    return message;
+};
