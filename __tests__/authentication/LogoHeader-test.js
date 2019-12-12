62a0401c44d1e7f302ad786545da1495e82bd6a1
gerryfletch <1997Gerry@gmail.com>
2019-12-12 14:51:13 +0000

    Removed unecessary import

--- __tests__/authentication/LogoHeader-test.js
+++ __tests__/authentication/LogoHeader-test.js
@@ -5,7 +5,6 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import {shallow} from "enzyme";
-import SubmitButton from "../../src/forms/SubmitButton";
 import LogoHeader from "../../src/authentication/LogoHeader";
 
 test('should maintain snapshot', () => expect(
