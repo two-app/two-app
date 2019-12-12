8660e02284d63f2cb2cd29a89b123089b5e266f3
gerryfletch <1997Gerry@gmail.com>
2019-12-12 14:31:41 +0000

    WRote tests for register screen

--- __tests__/forms/SubmitButton-test.js
+++ __tests__/forms/SubmitButton-test.js
@@ -8,22 +8,23 @@ import SubmitButton from "../../src/forms/SubmitButton";
 import shallow from "enzyme/shallow";
 
 test('should maintain snapshot', () => expect(
-    renderer.create(<SubmitButton text={"Submit"} onSubmit={() => console.log("Working")}/>)
+    renderer.create(<SubmitButton text={"Submit"} onSubmit={null}/>)
 ).toMatchSnapshot());
 
 test('should callback onSubmit when pressed', () => {
     const onSubmitFn = jest.fn();
-    const wrapper = shallow(<SubmitButton onSubmit={onSubmitFn} text={""}/>);
-    const clickable = wrapper.find('TouchableOpacity');
 
-    clickable.prop("onPress")();
+    shallow(<SubmitButton onSubmit={onSubmitFn} text={""}/>)
+        .find("TouchableOpacity")
+        .prop("onPress")(); // trigger press
 
     expect(onSubmitFn).toHaveBeenCalled();
 });
 
 test('should display given text', () => {
-    const wrapper = shallow(<SubmitButton onSubmit={null} text={"Test Submit"}/>);
-    const displayedText = wrapper.find("Text").render().text();
+    const displayedText = shallow(<SubmitButton onSubmit={null} text={"Test Submit"}/>)
+        .find("Text")
+        .render().text();
 
     expect(displayedText).toEqual("Test Submit");
 });
--- src/Colors.js
+++ src/Colors.js
@@ -3,7 +3,8 @@
 const Colors = {
     SALMON: "#EE7674",
     DARK_SALMON: "#ed615f",
-    FADED: "rgba(0, 0, 0, 0.20)",
+    LIGHT: "rgba(0, 0, 0, 0.1)",
+    FADED: "rgba(0, 0, 0, 0.2)",
     DARK: "rgba(0, 0, 0, 0.8)"
 };
 
