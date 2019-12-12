54eb3b5298ad4f8f4bc0244e2e47a4c73549c93f
gerryfletch <1997Gerry@gmail.com>
2019-12-12 23:03:59 +0000

    Updated valid green value to be Apples own

--- src/Colors.js
+++ src/Colors.js
@@ -6,7 +6,7 @@ const Colors = {
     LIGHT: "rgba(0, 0, 0, 0.1)",
     FADED: "rgba(0, 0, 0, 0.2)",
     DARK: "rgba(0, 0, 0, 0.8)",
-    VALID_GREEN: "#85CC73"
+    VALID_GREEN: "rgba(52, 199, 89, 1)"
 };
 
 export default Colors;
