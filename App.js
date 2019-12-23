32b93999a60e50e78e9a6ee9668ce2d762f90987
gerryfletch <1997Gerry@gmail.com>
2019-12-23 10:49:51 +0000

    Removed reference to create account screen, deleted file, and created connect code screen test file

--- App.js
+++ App.js
@@ -9,7 +9,6 @@ import LoginScreen from "./src/authentication/LoginScreen";
 import {createAppContainer} from "react-navigation";
 import RegisterScreen from "./src/authentication/RegisterScreen";
 import AcceptTermsScreen from "./src/authentication/register_workflow/AcceptTermsScreen";
-import CreateAccountScreen from "./src/authentication/register_workflow/CreateAccountScreen";
 import ConnectCodeScreen from "./src/authentication/register_workflow/ConnectCodeScreen";
 import {Provider} from "react-redux";
 import createReduxStore from "./src/state";
@@ -18,7 +17,6 @@ const navigator = createStackNavigator({
     Login: {screen: LoginScreen},
     Register: {screen: RegisterScreen},
     AcceptTermsScreen: {screen: AcceptTermsScreen},
-    CreateAccountScreen: {screen: CreateAccountScreen},
     ConnectCodeScreen: {screen: ConnectCodeScreen}
 }, {
     initialRouteName: "Register"
