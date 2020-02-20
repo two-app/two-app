2c3bea0cf88500a7c63fd0c48324dc51c5514fae
gerryfletch <1997Gerry@gmail.com>
2020-02-20 17:13:09 +0000

    Implemented JWT authorization header as interceptor in Gateway.

--- src/http/Gateway.js
+++ src/http/Gateway.js
@@ -1,8 +1,23 @@
-import Axios from 'axios';
+// @flow
+import Axios, {AxiosRequestConfig} from 'axios';
+import {store} from "../state/reducers";
 
 const Gateway = Axios.create({
     baseURL: 'http://localhost:8080/',
     timeout: 5000,
+});
+
+Gateway.interceptors.request.use((config: AxiosRequestConfig) => {
+    if (config.url === "/self" && config.method === "post") {
+        return config;
+    }
+
+    // apply JWT to outgoing request
+    const token = store.getState().authentication['accessToken'];
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+}, error => {
+    return Promise.reject(error);
 });
 
 export default Gateway;
