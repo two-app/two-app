0b6b83e311652118a86f47c9ea815d2257d7263a
gerryfletch <1997Gerry@gmail.com>
2020-02-21 00:07:23 +0000

    Loading screen responsible for routing based on async state, and tests.

---
+++ __tests__/LoadingScreen-test.js
@@ -0,0 +1,87 @@
+// @flow
+
+import 'react-native';
+import React from 'react';
+// Note: test renderer must be required after react-native.
+import {act, create} from 'react-test-renderer';
+import {LoadingScreen} from "../src/LoadingScreen";
+import {UnconnectedUser, User} from "../src/authentication/UserModel";
+import {Tokens} from "../src/authentication/AuthenticationModel";
+import {NavigationActions, StackActions} from "react-navigation";
+import {persistor} from "../src/state/reducers";
+
+describe('LoadingScreen', () => {
+
+    let tb: LoadingScreenTestBed;
+
+    beforeEach(() => tb = new LoadingScreenTestBed());
+
+    describe('With no auth', () => {
+        beforeEach(() => tb.build());
+
+        test('state should be cleared non async', () => expect(tb.clearStateFn).toHaveBeenCalled());
+
+        test('navigates to Register Screen', () => expect(tb.dispatchFn).toHaveBeenCalledWith(
+            StackActions.reset({
+                index: 0,
+                actions: [NavigationActions.navigate({routeName: "RegisterScreen"})]
+            })
+        ));
+
+        test('redux should be persisted to local storage', () => expect(tb.persistFn).toHaveBeenCalled());
+    });
+
+    describe('With Unconnected Auth', () => {
+        const stubUser = new UnconnectedUser(1, "testConnectCode");
+        const stubTokens = new Tokens("unconnectedAccess", "unconnectedRefresh");
+        beforeEach(() => tb.setUserProp(stubUser, stubTokens).setAuthProp(stubTokens).build());
+
+        test('navigates to Connect Code Screen', () => expect(tb.dispatchFn).toHaveBeenCalledWith(
+            StackActions.reset({
+                index: 0,
+                actions: [NavigationActions.navigate({routeName: "ConnectCodeScreen"})]
+            })
+        ));
+    });
+
+    describe('With Connected Auth', () => {
+        const stubUser = new User(1, 2, 3);
+        const stubTokens = new Tokens("connectedAccess", "connectedRefresh");
+        beforeEach(() => tb.setUserProp(stubUser, stubTokens).setAuthProp(stubTokens).build());
+
+        test('navigates to Home Screen', () => expect(tb.dispatchFn).toHaveBeenCalledWith(
+            StackActions.reset({
+                index: 0,
+                actions: [NavigationActions.navigate({routeName: "HomeScreen"})]
+            })
+        ));
+    });
+
+});
+
+class LoadingScreenTestBed {
+    dispatchFn = jest.fn();
+    clearStateFn = jest.fn();
+    userProp: null;
+    authProp: null;
+    persistFn = jest.spyOn(persistor, "persist");
+
+    setUserProp = (user: User | UnconnectedUser) => {
+        this.userProp = user;
+        return this;
+    };
+
+    setAuthProp = (auth: Tokens) => {
+        this.authProp = auth;
+        return this;
+    };
+
+    build = () => act(() => create(
+        <LoadingScreen
+            navigation={{dispatch: this.dispatchFn}}
+            clearState={this.clearStateFn}
+            user={this.userProp}
+            auth={this.authProp}
+        />
+    ));
+}
--- __tests__/home/HomeScreen-test.js
+++ __tests__/home/HomeScreen-test.js
@@ -4,16 +4,11 @@ import 'react-native';
 import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import {shallow} from "enzyme";
-import {AcceptTermsScreen} from "../../src/authentication/register_workflow/AcceptTermsScreen";
 import {HomeScreen} from "../../src/home/HomeScreen";
+import {User} from "../../src/authentication/UserModel";
 
 describe('HomeScreen', () => {
-    test('should maintain snapshot', () => expect(renderer.create(<AcceptTermsScreen
-        navigation={{getParam: jest.fn().mockReturnValue(HomeScreen)}}/>
-    )).toMatchSnapshot());
-});
+    const user = new User(1, 2, 3);
 
-class HomeScreenTestBed {
-    wrapper = shallow(<HomeScreen user={}/>);
-}
+    test('should maintain snapshot', () => expect(renderer.create(<HomeScreen user={user}/>)).toMatchSnapshot());
+});
