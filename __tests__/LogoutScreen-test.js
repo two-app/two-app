848c89c9ad41a7367c6821e2c1e5e448e89b4afe
gerryfletch <1997Gerry@gmail.com>
2020-02-22 12:17:27 +0000

    Fixed persistor in test

--- __tests__/LogoutScreen-test.js
+++ __tests__/LogoutScreen-test.js
@@ -5,17 +5,17 @@ import React from 'react';
 // Note: test renderer must be required after react-native.
 import {act, create} from 'react-test-renderer';
 import {LogoutScreen} from "../src/LogoutScreen";
+import {persistor} from "../src/state/reducers";
 
 describe('LogoutScreen', () => {
     const clearStateFn = jest.fn();
-    const persistFn = jest.fn();
+    const persistFn = jest.spyOn(persistor, "persist");
     const navigateFn = jest.fn();
 
     beforeAll(() => {
         act(() => create(
             <LogoutScreen
                 clearState={clearStateFn}
-                persistor={{persist: persistFn}}
                 navigation={{navigate: navigateFn}}
             />
         ));
