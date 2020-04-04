a42014de53d39e252628a1dad426a097df7f33c3
gerryfletch <1997Gerry@gmail.com>
2020-04-04 18:58:06 +0100

    Created image gallery

--- src/memories/new_memory/ContentInput.tsx
+++ src/memories/new_memory/ContentInput.tsx
@@ -1,14 +1,21 @@
-import ImagePicker, {Image} from 'react-native-image-crop-picker';
-import {InputCardButton} from '../../forms/InputCardButton';
-import {Text, View} from 'react-native';
+import ImagePicker, { Image, Options } from 'react-native-image-crop-picker';
+import { InputCardButton } from '../../forms/InputCardButton';
+import { Text, View } from 'react-native';
 import IonIcon from 'react-native-vector-icons/Ionicons';
 import Colors from '../../Colors';
 import React from 'react';
-import {FormStyle} from './FormStyles';
+import { FormStyle } from './FormStyles';
 
-const ContentInput = ({setContent}: { setContent: Function }) => {
+const ContentInput = ({ setContent }: { setContent: Function }) => {
     const selectMedia = () => {
-        ImagePicker.openPicker({multiple: true, maxFiles: 10}).then((value: Image | Image[]) => {
+        const options: Options = {
+            multiple: true,
+            maxFiles: 10,
+            compressImageMaxWidth: 1800,
+            compressImageMaxHeight: 1800
+        };
+        ImagePicker.openPicker(options).then((value: Image | Image[]) => {
+            console.log(value);
             Array.isArray(value) ? setContent(value) : setContent([value]);
         });
     };
@@ -16,11 +23,11 @@ const ContentInput = ({setContent}: { setContent: Function }) => {
     return (
         <InputCardButton style={FormStyle.card} onClick={() => selectMedia()}>
             <View style={FormStyle.iconContainer}>
-                <IonIcon name="md-images" style={{fontSize: 13, color: Colors.REGULAR}}/>
+                <IonIcon name="md-images" style={{ fontSize: 13, color: Colors.REGULAR }} />
             </View>
             <Text>Select images and videos</Text>
         </InputCardButton>
     );
 };
 
-export {ContentInput};
+export { ContentInput };
