dd72ecc8cfa062610a5e8917fb4448c6b319071c
gerryfletch <1997Gerry@gmail.com>
2020-04-10 09:55:01 +0100

    Fixed tests

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,9 @@ module.exports = {
     snapshotSerializers: [
         "enzyme-to-json/serializer"
     ],
+    setupFiles: [
+        "./node_modules/react-native-gesture-handler/jestSetup.js"
+    ],
     setupFilesAfterEnv: [
         "<rootDir>jest_test_setup.tsx"
     ]
