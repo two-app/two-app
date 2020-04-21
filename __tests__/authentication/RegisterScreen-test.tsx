9076898e2d8959efbb477dcd250b56838ff2dc9f
gerryfletch <1997Gerry@gmail.com>
2020-04-21 21:32:26 +0100

    Fixed and improved register screen tests

--- __tests__/authentication/RegisterScreen-test.tsx
+++ __tests__/authentication/RegisterScreen-test.tsx
@@ -2,80 +2,98 @@ import 'react-native';
 import React from 'react';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import {shallow, ShallowWrapper} from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import RegisterScreen from '../../src/authentication/RegisterScreen';
 import EmailValidator from '../../src/forms/EmailValidator';
-import {UserRegistration} from '../../src/authentication/register_workflow/UserRegistrationModel';
+import { UserRegistration } from '../../src/authentication/register_workflow/UserRegistrationModel';
 
-test('should maintain snapshot', () => expect(renderer.create(<RegisterScreen
-    navigation={{} as any}/>)
-).toMatchSnapshot());
+describe('RegisterScreen', () => {
+    test('should maintain snapshot', () => expect(renderer.create(<RegisterScreen
+        navigation={{} as any} />)
+    ).toMatchSnapshot());
 
-describe('Navigation Tests', () => {
-    test('it should navigate when Join Two is clicked', () => {
-        const navigate = jest.fn();
+    let tb: RegisterScreenTestBed;
 
-        shallow(<RegisterScreen navigation={{navigate} as any}/>).find('SubmitButton')
-            .prop<() => void>('onSubmit')();
+    beforeEach(() => tb = new RegisterScreenTestBed());
 
-        expect(navigate).toHaveBeenCalledWith('AcceptTermsScreen', {userRegistration: new UserRegistration()});
+    test('Login Button should navigate to LoginScreen', () => {
+        const btn: any = tb.wrapper.find("TouchableOpacity[data-testid=\'login-screen-button\']").first()
+
+        btn.prop('onPress')();
+    });
+
+    describe('Navigation Tests', () => {
+        test('it should navigate when submit button is clicked', () => {
+            tb.clickSubmitButton();
+
+            expect(tb.navigateFn).toHaveBeenCalledWith(
+                'AcceptTermsScreen', { userRegistration: new UserRegistration() }
+            );
+        });
+    });
+
+    describe('Form Validation Tests', () => {
+
+        describe('First Name', () => {
+            test('invalid with one character', () => tb.expectInput('First Name', 'a').toBe(false));
+            test('valid with three characters', () => tb.expectInput('First Name', 'abc').toBe(true));
+        });
+
+        describe('Last Name', () => {
+            test('invalid with 1 character', () => tb.expectInput('Last Name', 'a').toBe(false));
+            test('valid with 3 characters', () => tb.expectInput('Last Name', 'abc').toBe(true));
+        });
+
+        describe('Email', () => {
+            test('delegates to validEmail fn', () => {
+                EmailValidator.validateEmail = jest.fn().mockReturnValue(true);
+                tb.expectInput('Email', 'admin@two.com');
+                expect(EmailValidator.validateEmail).toHaveBeenCalledTimes(1);
+                expect(EmailValidator.validateEmail).toHaveBeenCalledWith('admin@two.com');
+            });
+        });
+
+        describe('Password', () => {
+            test('invalid with < 3 characters', () => tb.expectInput('Password', 'abc').toBe(false));
+            test('valid with > 3 characters', () => tb.expectInput('Password', 'abcd').toBe(true));
+            test('valid with secure password', () => tb.expectInput('Password', 'P?4Ot2ONz:IJO&%U').toBe(true));
+        });
+
+        describe('Fully Valid Form', () => {
+            test('should enable the submit button', () => {
+                expect(tb.wrapper.find('SubmitButton').prop('disabled')).toBe(true);
+
+                tb.changeInput('First Name', 'Gerry');
+                tb.changeInput('Last Name', 'Fletcher');
+                EmailValidator.validateEmail = jest.fn().mockReturnValue(true);
+                tb.changeInput('Email', 'admin@two.com');
+                tb.changeInput('Password', 'P?4Ot2ONz:IJO&%U');
+
+                expect(tb.wrapper.find('SubmitButton').prop('disabled')).toBe(false);
+            });
+        });
     });
 });
 
-describe('Form Validation Tests', () => {
+class RegisterScreenTestBed {
 
-    let wrapper: ShallowWrapper;
+    navigateFn = jest.fn();
+    dispatchFn = jest.fn();
+    wrapper = shallow(<RegisterScreen navigation={{ navigate: this.navigateFn, dispatch: this.dispatchFn } as any} />);
 
-    beforeEach(() => wrapper = shallow(<RegisterScreen navigation={{} as any}/>));
-
-    describe('First Name', () => {
-        test('invalid with one character', () => expectInput('First Name', 'a').toBe(false));
-        test('valid with three characters', () => expectInput('First Name', 'abc').toBe(true));
-    });
-
-    describe('Last Name', () => {
-        test('invalid with 1 character', () => expectInput('Last Name', 'a').toBe(false));
-        test('valid with 3 characters', () => expectInput('Last Name', 'abc').toBe(true));
-    });
-
-    describe('Email', () => {
-        test('delegates to validEmail fn', () => {
-            EmailValidator.validateEmail = jest.fn().mockReturnValue(true);
-            expectInput('Email', 'admin@two.com');
-            expect(EmailValidator.validateEmail).toHaveBeenCalledTimes(1);
-            expect(EmailValidator.validateEmail).toHaveBeenCalledWith('admin@two.com');
-        });
-    });
-
-    describe('Password', () => {
-        test('invalid with < 3 characters', () => expectInput('Password', 'abc').toBe(false));
-        test('valid with > 3 characters', () => expectInput('Password', 'abcd').toBe(true));
-        test('valid with secure password', () => expectInput('Password', 'P?4Ot2ONz:IJO&%U').toBe(true));
-    });
-
-    describe('Fully Valid Form', () => {
-        test('should enable the submit button', () => {
-            expect(wrapper.find('SubmitButton').prop('disabled')).toBe(true);
-
-            changeInput('First Name', 'Gerry');
-            changeInput('Last Name', 'Fletcher');
-            EmailValidator.validateEmail = jest.fn().mockReturnValue(true);
-            changeInput('Email', 'admin@two.com');
-            changeInput('Password', 'P?4Ot2ONz:IJO&%U');
-
-            expect(wrapper.find('SubmitButton').prop('disabled')).toBe(false);
-        });
-    });
-
-    const expectInput = (label: string, value: string) => {
-        const input = wrapper.find(`Input[label='${label}']`).first();
+    expectInput = (label: string, value: string) => {
+        const input = this.wrapper.find(`Input[label='${label}']`).first();
         const isValidFunction = input.prop<(v: string) => boolean>('isValid');
         return expect(isValidFunction(value));
     };
 
-    const changeInput = (label: string, value: string) => {
-        const input = wrapper.find(`Input[label='${label}']`).first();
+    clickSubmitButton = () => {
+        this.wrapper.find('SubmitButton').prop<() => void>('onSubmit')();
+    }
+
+    changeInput = (label: string, value: string) => {
+        const input = this.wrapper.find(`Input[label='${label}']`).first();
         const onChangeFn = input.prop<(v: string) => void>('onChange');
         onChangeFn(value);
     };
-});
+}
--- src/authentication/RegisterScreen.tsx
+++ src/authentication/RegisterScreen.tsx
@@ -1,66 +1,60 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Input from '../forms/Input';
-import {WrapperContainer} from '../views/View';
+import { WrapperContainer } from '../views/View';
 import LogoHeader from './LogoHeader';
 import SubmitButton from '../forms/SubmitButton';
-import UserRegistrationModel, {UserRegistration} from './register_workflow/UserRegistrationModel';
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RootStackParamList} from '../../Router';
+import UserRegistrationModel, { UserRegistration } from './register_workflow/UserRegistrationModel';
+import { StackNavigationProp } from '@react-navigation/stack';
+import { RootStackParamList } from '../../Router';
 import { Text, View } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Colors from '../Colors';
-import { CommonActions } from '@react-navigation/native';
+import { resetNavigate } from '../navigation/NavigationUtilities';
 
 type RegisterScreenProps = {
     navigation: StackNavigationProp<RootStackParamList, 'RegisterScreen'>
 };
 
-const RegisterScreen = ({navigation}: RegisterScreenProps) => {
+const RegisterScreen = ({ navigation }: RegisterScreenProps) => {
     const [userRegistration, setUserRegistration] = useState(new UserRegistration());
-
-    const navigateToLogin = () => {
-        navigation.dispatch(
-            CommonActions.reset({
-                index: 0,
-                routes: [{ name: 'LoginScreen' }]
-            })
-        );
-    }
 
     return (
         <WrapperContainer>
-            <LogoHeader heading="Create Account"/>
+            <LogoHeader heading="Create Account" />
 
-            <Input attributes={{placeholder: 'First Name', autoCompleteType: 'name'}}
-                   isValid={UserRegistrationModel.isFirstNameValid} label={'First Name'}
-                   onChange={firstName => setUserRegistration({...userRegistration, firstName})}
+            <Input attributes={{ placeholder: 'First Name', autoCompleteType: 'name' }}
+                isValid={UserRegistrationModel.isFirstNameValid} label={'First Name'}
+                onChange={firstName => setUserRegistration({ ...userRegistration, firstName })}
             />
 
-            <Input attributes={{placeholder: 'Last Name'}}
-                   isValid={UserRegistrationModel.isLastNameValid} label={'Last Name'}
-                   onChange={lastName => setUserRegistration({...userRegistration, lastName})}
+            <Input attributes={{ placeholder: 'Last Name' }}
+                isValid={UserRegistrationModel.isLastNameValid} label={'Last Name'}
+                onChange={lastName => setUserRegistration({ ...userRegistration, lastName })}
             />
 
-            <Input attributes={{placeholder: 'you@email.com', autoCompleteType: 'email', autoCapitalize: 'none'}}
-                   isValid={UserRegistrationModel.isEmailValid} label={'Email'}
-                   onChange={email => setUserRegistration({...userRegistration, email})}
+            <Input attributes={{ placeholder: 'you@email.com', autoCompleteType: 'email', autoCapitalize: 'none' }}
+                isValid={UserRegistrationModel.isEmailValid} label={'Email'}
+                onChange={email => setUserRegistration({ ...userRegistration, email })}
             />
 
-            <Input attributes={{placeholder: 'Secure Password', autoCompleteType: 'password', secureTextEntry: true}}
-                   isValid={UserRegistrationModel.isPasswordValid}
-                   label={'Password'}
-                   onChange={password => setUserRegistration({...userRegistration, password})}
+            <Input attributes={{ placeholder: 'Secure Password', autoCompleteType: 'password', secureTextEntry: true }}
+                isValid={UserRegistrationModel.isPasswordValid}
+                label={'Password'}
+                onChange={password => setUserRegistration({ ...userRegistration, password })}
             />
 
             <SubmitButton text="Join Two"
-                          onSubmit={() => navigation.navigate('AcceptTermsScreen', {userRegistration})}
-                          disabled={!UserRegistrationModel.isUserRegistrationValid(userRegistration)}
+                onSubmit={() => navigation.navigate('AcceptTermsScreen', { userRegistration })}
+                disabled={!UserRegistrationModel.isUserRegistrationValid(userRegistration)}
             />
 
-            <View style={{flexDirection: 'row', marginTop: 10}}>
-                <Text style={{color: Colors.REGULAR}}>Already a member?</Text>
-                <TouchableOpacity style={{marginLeft: 5}} onPress={navigateToLogin}>
-                    <Text style={{fontWeight: "bold", color: Colors.DARK}}>Sign In</Text>
+            <View style={{ flexDirection: 'row', marginTop: 10 }}>
+                <Text style={{ color: Colors.REGULAR }}>Already a member?</Text>
+                <TouchableOpacity style={{ marginLeft: 5 }}
+                    onPress={() => resetNavigate('LoginScreen', navigation)}
+                    data-testid={'login-screen-button'}
+                >
+                    <Text style={{ fontWeight: "bold", color: Colors.DARK }}>Sign In</Text>
                 </TouchableOpacity>
             </View>
         </WrapperContainer>
