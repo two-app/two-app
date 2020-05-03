08705604455ce7918fcf72db8802cd37f552fe67
gerryfletch <1997Gerry@gmail.com>
2020-05-03 21:40:25 +0100

    Modified submit button to use Button component

--- src/authentication/LoginScreen.tsx
+++ src/authentication/LoginScreen.tsx
@@ -50,7 +50,7 @@ const LoginScreen = ({ navigation, storeUser, storeUnconnectedUser, storeTokens 
     }
 
     return (
-        <ScrollContainer isLoading={submitted}>
+        <ScrollContainer isLoading={submitted} keyboardShouldPersistTaps='always' keyboardDismissMode='on-drag'>
             <LogoHeader heading="Sign In" />
 
             <Input attributes={{ placeholder: 'you@email.com', autoCompleteType: 'email', autoCapitalize: 'none' }}
