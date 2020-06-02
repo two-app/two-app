6ea3ecf35f09f856dc70a8641ffa017322d8ac98
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2020-06-02 11:54:30 +0100

    Doubled vertical gesture width for navigation. (#73)

--- Router.tsx
+++ Router.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
+import {createStackNavigator} from '@react-navigation/stack';
 import LoadingScreen from './src/LoadingScreen';
 import LogoutScreen from './src/LogoutScreen';
 import LoginScreen from './src/authentication/LoginScreen';
@@ -7,49 +7,66 @@ import RegisterScreen from './src/authentication/RegisterScreen';
 import AcceptTermsScreen from './src/authentication/register_workflow/AcceptTermsScreen';
 import ConnectCodeScreen from './src/authentication/register_workflow/ConnectCodeScreen';
 import HomeScreen from './src/home/HomeScreen';
-import { NewMemoryScreen } from './src/memories/new_memory/NewMemoryScreen';
-import { UserRegistration } from './src/authentication/register_workflow/UserRegistrationModel';
-import { MemoryScreen } from './src/memories/memory/MemoryScreen';
-import { Memory } from './src/memories/MemoryModels';
-import { NewTagScreen } from './src/tags/NewTagScreen';
-import { Tag } from './src/tags/Tag';
-import { SearchScreen } from './src/search/SearchScreen';
+import {NewMemoryScreen} from './src/memories/new_memory/NewMemoryScreen';
+import {UserRegistration} from './src/authentication/register_workflow/UserRegistrationModel';
+import {MemoryScreen} from './src/memories/memory/MemoryScreen';
+import {Memory} from './src/memories/MemoryModels';
+import {NewTagScreen} from './src/tags/NewTagScreen';
+import {Tag} from './src/tags/Tag';
+import {SearchScreen} from './src/search/SearchScreen';
 import ProfileScreen from './src/user/ProfileScreen';
 
 export type RootStackParamList = {
-    LoadingScreen: undefined;
-    LogoutScreen: undefined;
-    LoginScreen: undefined;
-    RegisterScreen: undefined;
-    AcceptTermsScreen: { userRegistration: UserRegistration };
-    ConnectCodeScreen: undefined;
-    HomeScreen: undefined;
-    NewMemoryScreen: undefined;
-    MemoryScreen: { memory: Memory };
-    NewTagScreen: { onSubmit: (tag: Tag) => void },
-    SearchScreen: undefined;
-    ProfileScreen: undefined;
+  LoadingScreen: undefined;
+  LogoutScreen: undefined;
+  LoginScreen: undefined;
+  RegisterScreen: undefined;
+  AcceptTermsScreen: {userRegistration: UserRegistration};
+  ConnectCodeScreen: undefined;
+  HomeScreen: undefined;
+  NewMemoryScreen: undefined;
+  MemoryScreen: {memory: Memory};
+  NewTagScreen: {onSubmit: (tag: Tag) => void};
+  SearchScreen: undefined;
+  ProfileScreen: undefined;
 };
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const AppStack = () => (
-    <Stack.Navigator initialRouteName="LoadingScreen" headerMode={'none'}
-        screenOptions={{ cardStyle: { backgroundColor: "white" }, headerShown: false }}
-    >
-        <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
-        <Stack.Screen name="LogoutScreen" component={LogoutScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
-        <Stack.Screen name="AcceptTermsScreen" component={AcceptTermsScreen} />
-        <Stack.Screen name="ConnectCodeScreen" component={ConnectCodeScreen} />
-        <Stack.Screen name="HomeScreen" component={HomeScreen} options={{animationEnabled: false}} />
-        <Stack.Screen name="NewMemoryScreen" component={NewMemoryScreen} />
-        <Stack.Screen name="MemoryScreen" component={MemoryScreen} />
-        <Stack.Screen name="NewTagScreen" component={NewTagScreen} />
-        <Stack.Screen name="SearchScreen" component={SearchScreen} />
-        <Stack.Screen name="ProfileScreen" component={ProfileScreen} options={{animationEnabled: false}}/>
-    </Stack.Navigator>
+  <Stack.Navigator
+    initialRouteName="LoadingScreen"
+    headerMode={'none'}
+    screenOptions={{
+      cardStyle: {
+        backgroundColor: 'white',
+      },
+      headerShown: false,
+      gestureResponseDistance: {
+        vertical: 50,
+      },
+    }}>
+    <Stack.Screen name="LoadingScreen" component={LoadingScreen} />
+    <Stack.Screen name="LogoutScreen" component={LogoutScreen} />
+    <Stack.Screen name="LoginScreen" component={LoginScreen} />
+    <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+    <Stack.Screen name="AcceptTermsScreen" component={AcceptTermsScreen} />
+    <Stack.Screen name="ConnectCodeScreen" component={ConnectCodeScreen} />
+    <Stack.Screen
+      name="HomeScreen"
+      component={HomeScreen}
+      options={{animationEnabled: false}}
+    />
+    <Stack.Screen name="NewMemoryScreen" component={NewMemoryScreen} />
+    <Stack.Screen name="MemoryScreen" component={MemoryScreen} />
+    <Stack.Screen name="NewTagScreen" component={NewTagScreen} />
+    <Stack.Screen name="SearchScreen" component={SearchScreen} />
+    <Stack.Screen
+      name="ProfileScreen"
+      component={ProfileScreen}
+      options={{animationEnabled: false}}
+    />
+  </Stack.Navigator>
 );
 
-export { AppStack };
+export {AppStack};
