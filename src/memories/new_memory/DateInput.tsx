5ad4ef0b1a349ff13a0ec808cb454210a69f9f32
Gerry Fletcher <gerryfletch@users.noreply.github.com>
2020-06-04 20:16:52 +0100

    Date + Time Picker for New Memories (#76)

--- src/memories/new_memory/DateInput.tsx
+++ src/memories/new_memory/DateInput.tsx
@@ -1,46 +1,90 @@
 import React, {useState} from 'react';
-import {Keyboard, Text, View} from 'react-native';
+import {Keyboard, Text, View, Platform} from 'react-native';
 import AntIcon from 'react-native-vector-icons/AntDesign';
 import Colors from '../../Colors';
 import DateTimePickerModal from 'react-native-modal-datetime-picker';
 import {FormStyle} from './FormStyles';
 import moment from 'moment';
-import { TouchableCard } from '../../forms/Card';
+import {TouchableCard} from '../../forms/Card';
 
-const DateInput = ({setDate}: { setDate: Function }) => {
-    const [isVisible, setVisibility] = useState(false);
-    const [pickerValue, setPickerValue] = useState<number | null>(null);
-
-    const openPicker = () => {
-        Keyboard.dismiss();
-        setVisibility(true);
-    };
-
-    const selectDate = (date: Date) => {
-        setVisibility(false);
-        setPickerValue(date.getTime());
-        setDate(date.getTime());
-    };
-
-    return (<>
-        <TouchableCard style={FormStyle.card} onPress={openPicker}>
-            <View style={FormStyle.iconContainer}>
-                <AntIcon name="calendar" style={{fontSize: 13, color: Colors.REGULAR}}/>
-            </View>
-            {pickerValue == null && <Text style={{color: Colors.REGULAR}}>When it took place...</Text>}
-            {pickerValue != null && <Text
-                style={{color: Colors.DARK}}>{moment(pickerValue).format("LL")}</Text>}
-        </TouchableCard>
-        <DateTimePickerModal
-            isVisible={isVisible}
-            minimumDate={new Date(Date.UTC(0, 0, 0, 0, 0, 0))}
-            maximumDate={new Date()}
-            mode="date"
-            onConfirm={selectDate}
-            onCancel={() => setVisibility(false)}
-            display='calendar'
-        />
-    </>);
+type DateTimePickerProps = {
+  onSetDateTime: (datetime: number) => void;
 };
 
-export {DateInput};
+/**
+ * Utility for the user to select a date and time.
+ * On iOS, there is no native datetime picker, so a two-step modal is used. The intermediary 'date' value
+ * is stored as the `pickerValue`.
+ */
+export const DateTimePicker = ({onSetDateTime}: DateTimePickerProps) => {
+  const [isVisible, setVisibility] = useState(false);
+  const [pickerValue, setPickerValue] = useState<number>();
+  const [date, setDate] = useState<number>();
+  const [selecting, setSelecting] = useState<'date' | 'time'>('date');
+
+  const openPicker = () => {
+    Keyboard.dismiss();
+    setVisibility(true);
+  };
+
+  const reset = () => {
+    setVisibility(false);
+    setSelecting('date');
+    setPickerValue(undefined);
+  };
+
+  const updateDate = (date: number) => {
+    setDate(date);
+    onSetDateTime(date);
+    reset();
+  };
+
+  return (
+    <>
+      <TouchableCard style={FormStyle.card} onPress={openPicker}>
+        <View style={FormStyle.iconContainer}>
+          <AntIcon
+            name={'calendar'}
+            style={{fontSize: 13, color: Colors.REGULAR}}
+          />
+        </View>
+        {date == null && (
+          <Text style={{color: Colors.REGULAR}}>When it took place...</Text>
+        )}
+        {date != null && (
+          <Text style={{color: Colors.DARK}}>{moment(date).format('LLL')}</Text>
+        )}
+      </TouchableCard>
+      {Platform.OS === 'android' ? (
+        <DateTimePickerModal
+          isVisible={isVisible}
+          maximumDate={new Date()}
+          mode="datetime"
+          onConfirm={(selectedDate: Date) => updateDate(selectedDate.getTime())}
+          onCancel={reset}
+        />
+      ) : (
+        <DateTimePickerModal
+          isVisible={isVisible}
+          maximumDate={new Date()}
+          mode={selecting}
+          headerTextIOS={
+            selecting === 'date' ? 'Pick the date' : 'Pick the time'
+          }
+          onConfirm={(selectedDate: Date) => {
+            if (selecting === 'date') {
+              setPickerValue(selectedDate.getTime());
+              setSelecting('time');
+            } else {
+              const onlyDate = moment(pickerValue).format('YYYY-MM-DD');
+              const onlyTime = moment(selectedDate).format('HH:mm:00');
+              const datetime = moment(`${onlyDate} ${onlyTime}`);
+              updateDate(datetime.valueOf());
+            }
+          }}
+          onCancel={reset}
+        />
+      )}
+    </>
+  );
+};
--- src/memories/new_memory/NewMemoryScreen.tsx
+++ src/memories/new_memory/NewMemoryScreen.tsx
@@ -1,76 +1,90 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
-import { ScrollContainer } from '../../views/View';
-import { Image as ImageType } from 'react-native-image-crop-picker';
-import { Heading } from '../../home/Heading';
+import {StackNavigationProp} from '@react-navigation/stack';
+import React, {useState} from 'react';
+import {Text, View} from 'react-native';
+import {Image as ImageType} from 'react-native-image-crop-picker';
+import {RootStackParamList} from '../../../Router';
 import SubmitButton from '../../forms/SubmitButton';
+import {Heading} from '../../home/Heading';
+import {ErrorResponse} from '../../http/Response';
+import {resetNavigate} from '../../navigation/NavigationUtilities';
+import {SelectTag} from '../../tags/SelectTag';
+import {Tag} from '../../tags/Tag';
+import {ScrollContainer} from '../../views/View';
+import {
+  isMemoryUploadValid,
+  MemoryUpload,
+  uploadMemory,
+} from '../MemoryService';
+import {ContentInput} from './ContentInput';
+import {ContentPreview} from './ContentPreview';
+import {DateTimePicker} from './DateInput';
+import {LocationInput} from './LocationInput';
 import TitleInput from './TitleInput';
-import { LocationInput } from './LocationInput';
-import { DateInput } from './DateInput';
-import { ContentInput } from './ContentInput';
-import { ContentPreview } from './ContentPreview';
-import { isMemoryUploadValid, MemoryUpload, uploadMemory } from '../MemoryService';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { RootStackParamList } from '../../../Router';
-import { SelectTag } from '../../tags/SelectTag';
-import { Tag } from '../../tags/Tag';
-import { resetNavigate } from '../../navigation/NavigationUtilities';
-import { ErrorResponse } from '../../http/Response';
 
 type NewMemoryScreenProps = {
-    navigation: StackNavigationProp<RootStackParamList, 'NewMemoryScreen'>
-}
-
-const NewMemoryScreen = ({ navigation }: NewMemoryScreenProps) => {
-    const [loading, setLoading] = useState({ isLoading: false, percentage: 0 });
-    const [uploadError, setUploadError] = useState<string>();
-    const [formState, setFormState] = useState<MemoryUpload>({
-        title: '',
-        location: '',
-        date: Date.now(),
-        content: [],
-        tag: undefined
-    });
-
-    const setTitle = (title: string) => setFormState({ ...formState, title });
-    const setLocation = (location: string) => setFormState({ ...formState, location });
-    const setDate = (date: number) => setFormState({ ...formState, date });
-    const setTagId = (tid: undefined | number) => setFormState({ ...formState, tag: tid });
-    const setContent = (content: ImageType[]) => setFormState({ ...formState, content: content });
-
-    const createMemory = () => {
-        setLoading({ isLoading: true, percentage: 0 });
-        uploadMemory(formState, percentage => setLoading({ isLoading: true, percentage })).then(() => {
-            resetNavigate('HomeScreen', navigation);
-        }).catch((e: ErrorResponse) => {
-            setUploadError(e.reason);
-        }).finally(() => {
-            setLoading({ isLoading: false, percentage: 0 })
-        });
-    };
-
-    return (
-        <ScrollContainer isLoading={loading.isLoading} loadingPercentage={loading.percentage}>
-            <View>
-                <Heading>New Memory</Heading>
-
-                <TitleInput setTitle={setTitle} />
-                <LocationInput setLocation={setLocation} />
-                <DateInput setDate={setDate} />
-                <SelectTag onTagChange={
-                    (tag: undefined | Tag) => setTagId(tag != null ? tag.tid : undefined)
-                } />
-                <ContentInput setContent={setContent} />
-                {formState.content.length > 0 && <ContentPreview content={formState.content} />}
-
-                <SubmitButton onSubmit={createMemory} text="Create Memory" disabled={!isMemoryUploadValid(formState)} />
-
-                {uploadError != null &&
-                    <Text>{uploadError}</Text>
-                }
-            </View>
-        </ScrollContainer>
-    );
+  navigation: StackNavigationProp<RootStackParamList, 'NewMemoryScreen'>;
 };
 
-export { NewMemoryScreen };
+const NewMemoryScreen = ({navigation}: NewMemoryScreenProps) => {
+  const [loading, setLoading] = useState({isLoading: false, percentage: 0});
+  const [uploadError, setUploadError] = useState<string>();
+  const [formState, setFormState] = useState<MemoryUpload>({
+    title: '',
+    location: '',
+    date: Date.now(),
+    content: [],
+    tag: undefined,
+  });
+
+  const setTitle = (title: string) => setFormState({...formState, title});
+  const setLocation = (location: string) =>
+    setFormState({...formState, location});
+  const setDate = (date: number) => setFormState({...formState, date});
+  const setTagId = (tid: undefined | number) =>
+    setFormState({...formState, tag: tid});
+  const setContent = (content: ImageType[]) =>
+    setFormState({...formState, content: content});
+
+  const createMemory = () => {
+    setLoading({isLoading: true, percentage: 0});
+    uploadMemory(formState, (percentage: number) =>
+      setLoading({isLoading: true, percentage}),
+    )
+      .then(() => resetNavigate('HomeScreen', navigation))
+      .catch((e: ErrorResponse) => setUploadError(e.reason))
+      .finally(() => setLoading({isLoading: false, percentage: 0}));
+  };
+
+  return (
+    <ScrollContainer
+      isLoading={loading.isLoading}
+      loadingPercentage={loading.percentage}>
+      <View>
+        <Heading>New Memory</Heading>
+
+        <TitleInput setTitle={setTitle} />
+        <LocationInput setLocation={setLocation} />
+        <DateTimePicker onSetDateTime={setDate} />
+        <SelectTag
+          onTagChange={(tag: undefined | Tag) =>
+            setTagId(tag != null ? tag.tid : undefined)
+          }
+        />
+        <ContentInput setContent={setContent} />
+        {formState.content.length > 0 && (
+          <ContentPreview content={formState.content} />
+        )}
+
+        <SubmitButton
+          onSubmit={createMemory}
+          text="Create Memory"
+          disabled={!isMemoryUploadValid(formState)}
+        />
+
+        {uploadError != null && <Text>{uploadError}</Text>}
+      </View>
+    </ScrollContainer>
+  );
+};
+
+export {NewMemoryScreen};
