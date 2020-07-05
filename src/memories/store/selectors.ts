d80232bcdaa3f1f60af240fdfab3783954e075fa
gerryfletch <1997Gerry@gmail.com>
2020-07-05 17:50:47 +0100

    Removed console logs

--- src/memories/store/selectors.ts
+++ src/memories/store/selectors.ts
@@ -9,8 +9,5 @@ export const selectMemory = (
   memoryState: MemoryState,
   mid: number,
 ): Memory | undefined => {
-  console.log("Selecting memory by mid " + mid);
-  console.log("Base state:");
-  console.log(memoryState);
   return memoryState.allMemories.find((m) => m.id === mid);
 };
