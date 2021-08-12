3cf4d578e13617c687a52b2eda225c8c2dedeaa9
gerryfletch <1997Gerry@gmail.com>
2021-08-12 08:40:42 +0100

    React Native 0.64.0 Upgrade

--- jest.config.js
+++ jest.config.js
@@ -4,8 +4,6 @@ module.exports = {
   snapshotSerializers: ['enzyme-to-json/serializer'],
   setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   setupFilesAfterEnv: ['<rootDir>jest_test_setup.tsx'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(jest-)?react-native|@react-native-community|@react-navigation)',
-  ],
+  transformIgnorePatterns: [],
   modulePathIgnorePatterns: ['<rootDir>/__tests__/utils/'],
 };
